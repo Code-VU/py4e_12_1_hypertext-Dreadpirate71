34975d273645b88bf087e498e116c635b5b63aaa
James Remus <James.Remus@veteransunited.com>
2022-05-03 18:59:45 -0500

    print data.decode

--- socket1.py
+++ socket1.py
@@ -3,7 +3,7 @@ import http.client
 from typing import Counter
 
 def getWebData():
-    """mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mysock.connect(('data.pr4e.org', 80))
     cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
     mysock.send(cmd)
@@ -20,10 +20,12 @@ def getWebData():
         #print(data_lines)
 
     mysock.close()
+    
     """
     conn = http.client.HTTPConnection("data.pr4e.org", 80)
     conn.request("GET", "/intro-short.txt")
     reply1 = conn.getresponse()
     header_dict = reply1.getheaders()
     print ('Why should you learn to write programs?')
+    """
 getWebData()
