bf49c1b96c3c8ea3a52f0e07de7ac33d3e665c1d
James Remus <James.Remus@veteransunited.com>
2022-05-03 18:27:42 -0500

    header info dictionary using http.client

--- socket1.py
+++ socket1.py
@@ -1,16 +1,29 @@
 import socket
+import http.client
+from typing import Counter
 
 def getWebData():
-    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    """mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mysock.connect(('data.pr4e.org', 80))
-    cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+    cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
     mysock.send(cmd)
-
+    count = 0
     while True:
         data = mysock.recv(512)
         if len(data) < 1:
             break
+        data_string = data.decode()
+        data_lines = data_string.splitlines()
+        #count = count + 1
         print(data.decode(),end='')
+        #print(data_string)
+        #print(data_lines)
 
     mysock.close()
-##getWebData()
+    """
+    conn = http.client.HTTPConnection("data.pr4e.org", 80)
+    conn.request("GET", "/intro-short.txt")
+    r1 = conn.getresponse()
+    dict = r1.getheaders()
+    print (dict)
+getWebData()
